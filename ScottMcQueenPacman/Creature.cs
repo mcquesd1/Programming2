4ca2edb01b5f18bf09a98bca31224697845b23f1
mcquesd1 <mcquesd1@student.op.ac.nz>
2015-10-29 20:24:02 +1300

    Worked on Creature Class

--- ScottMcQueenPacman/Creature.cs
+++ ScottMcQueenPacman/Creature.cs
@@ -3,19 +3,35 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ScottMcQueenPacman
 {
     public abstract class Creature
     {
-        protected Bitmap[] images;
+        protected Maze maze;
+        protected Bitmap image1;
+        protected Bitmap image2;
         protected Point position;
 
-        public Creature()
-        { 
-        
+        public Creature(String fileName1,String fileName2,Maze maze, Point position)
+        {
+            image1 = new Bitmap(fileName1);
+            image2 = new Bitmap(fileName2);
+            this.maze = maze;
+            this.position = position;
         }
-
+        public DataGridViewCell GetMazeCellForPosition(Point p)
+        {
+            return maze.Rows[p.Y].Cells[p.X];
+        }
+        public virtual void draw()
+        {
+            GetMazeCellForPosition(position).Value = image1;
+            Thread.Sleep(100);
+            GetMazeCellForPosition(position).Value = image2;
+        }
     }
 }
